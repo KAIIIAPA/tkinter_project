80af7867cfd5e470ec3f3f7710b8e9101e164bb1
Ildar <kaiiiapa1613@gmail.com>
2024-10-11 14:07:16 +0300

    Ваша задача:
    Добавление инструмента в виде кнопки "Ластик"
    
    Реализация:
    
    Добавьте кнопку "Ластик" (tk.Button), и при её нажатии устанавливайте self.pen_color в цвет фона ("white"). Убедитесь, что при переключении обратно на инструмент кисти восстанавливается предыдущий цвет.

--- main.py
+++ main.py
@@ -22,8 +22,11 @@ class DrawingApp:
         self.canvas.bind('<B1-Motion>', self.paint)
         self.canvas.bind('<ButtonRelease-1>', self.reset)
 
-
     def setup_ui(self):
+        '''
+        :return: Отображение панели инструментов (выбор цвета кисти, выбор размера кисти, очистка экрана,
+        сохранение рисунка и применение ластика)
+        '''
         control_frame = tk.Frame(self.root)
         control_frame.pack(fill=tk.X)
 
@@ -44,10 +47,27 @@ class DrawingApp:
         option_menu.pack(side=tk.LEFT)
 
         # Добавляем обработчик события для обновления размера кисти при изменении выбора
+        self.color_pen_previous = ''
         self.selected_brush_size.trace('w', self.update_brush_size)
+        self.eraser_button = tk.Button(control_frame, text="Ластик", command=lambda: self.set_eraser())
+        self.eraser_button.pack(side='left')
 
+    def set_eraser(self):
+        '''
+        :return: Возвращает ластик или кисть
+        '''
+        if self.eraser_button['text'] == "Ластик":
+            self.color_pen_previous = self.pen_color
+            self.pen_color = 'white'
+            self.eraser_button['text'] = "Кисть"
+        else:
+            self.pen_color = self.color_pen_previous
+            self.eraser_button['text'] = "Ластик"
 
     def update_brush_size(self, name, index, mode):
+        """
+        :return: Возвращает необходимый размер кисти
+        """
         try:
             size = int(self.selected_brush_size.get())
             self.selected_brush_size.set(size)
@@ -55,6 +75,9 @@ class DrawingApp:
             pass
 
     def paint(self, event):
+        """
+        :return: Возвращает в выбранном цвете проведенный контур
+        """
         if self.last_x and self.last_y:
             self.canvas.create_line(self.last_x, self.last_y, event.x, event.y,
                                     width=self.selected_brush_size.get(), fill=self.pen_color,
@@ -66,17 +89,32 @@ class DrawingApp:
         self.last_y = event.y
 
     def reset(self, event):
+        """
+        :param event:
+        :return: Метод, который срабатывает при отпускании кнопки мыши.
+        Он обнуляет последние известные координаты (self.last_x, self.last_y),
+        предотвращая дальнейшее рисование до следующего щелчка мышью.
+        """
         self.last_x, self.last_y = None, None
 
     def clear_canvas(self):
+        """
+        :return: Отчищает рабочую область
+        """
         self.canvas.delete("all")
         self.image = Image.new("RGB", (600, 400), "white")
         self.draw = ImageDraw.Draw(self.image)
 
     def choose_color(self):
+        """
+        :return: Возвращает выбранный цвет кисти
+        """
         self.pen_color = colorchooser.askcolor(color=self.pen_color)[1]
 
     def save_image(self):
+        """
+        :return: Сохраняет рисунок
+        """
         file_path = filedialog.asksaveasfilename(filetypes=[('PNG files', '*.png')])
         if file_path:
             if not file_path.endswith('.png'):
